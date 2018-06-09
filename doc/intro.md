58bf2796f186fd0e77f7130aff49e42545f6eb65
Mark Garland <mgarland@soltra.com>
2018-06-09 12:36:08 -0400

    created project with 'lein new app'

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to gr
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/gr/core_test.clj
@@ -0,0 +1,7 @@
+(ns gr.core-test
+  (:require [clojure.test :refer :all]
+            [gr.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
