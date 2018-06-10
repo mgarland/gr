1838918eecf64349ad42f30ecd4590ad57876f6e
Mark Garland <mgarland@soltra.com>
2018-06-09 21:33:39 -0400

    added tests for command line args and usage statement

--- src/gr/core.clj
+++ src/gr/core.clj
@@ -1,7 +1,29 @@
 (ns gr.core
   (:gen-class))
 
+;;
+;; command line argument validation
+;;
+(defn usage []
+  (do
+    (println "Usage: file-name sort-option")
+    (println "   file-name: full path to file to load")
+    (println "   sort-option: 1 - sort by Gender asc then by LastName asc")
+    (println "                2 - sort by DateOfBirth asc")
+    (println "                3 - sort by LastName desc")))
+
+(defn validate-sort-option [val]
+  (#{1 2 3} val))
+
+
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [[file-name sort-option] args
+        is-sort-option-valid? (validate-sort-option sort-option)]
+    (if is-sort-option-valid?
+        nil
+        (usage))))
+
+
--- test/gr/core_test.clj
+++ test/gr/core_test.clj
@@ -2,6 +2,12 @@
   (:require [clojure.test :refer :all]
             [gr.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest sort-option-validation
+  (testing "Test sort-option arg"
+    (is (= (validate-sort-option nil) (usage)))
+    (is (= (validate-sort-option "0") (usage)))
+    (is (= (validate-sort-option "1") nil))
+    (is (= (validate-sort-option "2") nil))
+    (is (= (validate-sort-option "3") nil))
+    (is (= (validate-sort-option "4") (usage)))
+    (is (= (validate-sort-option "a") (usage)))))
